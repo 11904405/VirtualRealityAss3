3834300cdefef2b08a97f5a276e56a7fdb759c31
11904405 <thomas.hellweger10@gmail.com>
2024-11-12 14:51:31 +0100

    changed accessing of lefthand righthand and camera in prefab

--- Group16_Assignment3/Assets/Resources/Scripts/SynchronizingTransformOfObjects.cs
+++ Group16_Assignment3/Assets/Resources/Scripts/SynchronizingTransformOfObjects.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SynchronizingPositionOfObjects : MonoBehaviour
+public class SynchronizingTransformOfObjects : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public string tagOfPositionToSync;
+    private GameObject objectToSync;
+
     void Start()
     {
+        objectToSync = GameObject.FindGameObjectWithTag(tagOfPositionToSync);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.position = objectToSync.transform.position;
+        this.transform.rotation = objectToSync.transform.rotation;
     }
 }
